a9f4c554093214838ca0329e4f4af512434c94ea
Christopher Andrews <godarklight@gmail.com>
2021-09-26 12:15:07 +1000

    Initial commit

---
+++ Memgraph/MemgraphMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using DarkLog;
+
+namespace Memgraph
+{
+    public class MemgraphMain : MonoBehaviour
+    {
+        ModLog log;
+        MemgraphWindow window;
+
+        public void Start()
+        {
+            log = new ModLog("Memgraph");
+            log.Log("Memgraph Start!");
+            window = new MemgraphWindow();
+            window.Start();
+            DontDestroyOnLoad(this);
+        }
+
+        public void Update()
+        {
+            window.Update();
+        }
+
+        public void OnGUI()
+        {
+            window.Draw();
+        }
+    }
+}
